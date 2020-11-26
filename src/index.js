f610c7a883eb75e0c4f6a873c3d6af74d78aaa6f
Evan Meeks <evan.meeks@gmail.com>
2020-11-26 06:48:27 -0600

    change

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-var winObservers2 = () => {
-  document.getElementsByTagName("html").style = "overflow:scroll; margin: 20px";
-  document.querySelectorAll("a").forEach((item) => {
-    item.addEventListener("click", () => {
-      const id = item.href.split("(")[1].split(",")[0];
-      console.log("click", id);
-      var img = document.createElement("img");
-      img.src = `https://snap.mfcimg.com/snapimg/955/640x480/mfc_1${id}?no-cache=`;
-      var div = (document.createElement(
-        "div"
-      ).innerHTML = `<div id="${id}"><span onClick="document.getElementById(${id}).remove()">x</span><img src="${img.src}" alt='as' /></div>`);
+document.getElementsByTagName("html").style = "overflow:scroll; margin: 20px";
+const links = document.querySelectorAll("a");
 
-      document.body.appendChild(div);
-    });
-  });
-  window.document.getElementsByTagName("html")[0].style =
-    "overflow:scroll; height:101vh";
-  window.document.getElementsByTagName("body")[0].style =
-    "overflow:scroll; height:101vh";
-};
-winObservers2();
+links.addEventListener("click", function (e) {
+  const id = this.href.split("(")[1].split(",")[0];
+  console.log("click", id);
+  var img = document.createElement("img");
+  img.src = `https://snap.mfcimg.com/snapimg/955/640x480/mfc_1${id}?no-cache=`;
+  var div = (document.createElement(
+    "div"
+  ).innerHTML = `<div id="${id}"><span onClick="document.getElementById(${id}).remove()">x</span><img src="${img.src}" alt='as' /></div>`);
+
+  document.body.appendChild(div);
+});
+window.document.getElementsByTagName("html")[0].style =
+  "overflow:scroll; height:101vh";
+window.document.getElementsByTagName("body")[0].style =
+  "overflow:scroll; height:101vh";
