2c01f4ad79d1cca1f897c58896a2346fe3640340
Evan Meeks <evan.meeks@gmail.com>
2020-11-26 05:15:40 -0600

    winObservers2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const winObservers = () => {
+var winObservers2 = () => {
   document.getElementsByTagName("html").style = "overflow:scroll; margin: 20px";
   document.querySelectorAll("a").forEach((item) => {
     item.addEventListener("click", () => {
@@ -6,8 +6,11 @@ const winObservers = () => {
       console.log("click", id);
       var img = document.createElement("img");
       img.src = `https://snap.mfcimg.com/snapimg/955/640x480/mfc_1${id}?no-cache=`;
+      var div = (document.createElement(
+        "div"
+      ).innerHTML = `<div id="${id}"><span onClick="document.getElementById(${id}).remove()">x</span><img src="${img.src}" alt='as' /></div>`);
 
-      document.body.appendChild(img);
+      document.body.appendChild(div);
     });
   });
   window.document.getElementsByTagName("html")[0].style =
@@ -15,4 +18,4 @@ const winObservers = () => {
   window.document.getElementsByTagName("body")[0].style =
     "overflow:scroll; height:101vh";
 };
-winObservers();
+winObservers2();
